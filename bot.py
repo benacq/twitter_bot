ab3a1098cd47a2ecc835aab9bcfa94ba91720d9b
darksidedev44 <bekifof492@lerbhe.com>
2020-06-21 17:27:55 +0100

    initial commit

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ CONSUMER_SECRET = os.environ.get('TWITTER_API_SECRET')  # replace TWITTER_API_SE
 ACCESS_KEY = os.environ.get('TWITTER_ACCESS_TOKEN_KEY')  # replace TWITTER_ACCESS_TOKEN_KEY with you api key
 ACCESS_SECRET = os.environ.get('TWITTER_ACCESS_TOKEN_SECRET')  # replace TWITTER_ACCESS_TOKEN_SECRET with you api key
 
-#TEST HASHTAGS
+# TEST HASHTAGS
 hash_tags = ["#darksidedev", "#darksidedev1", "#darksidedev2", "#darksidedev3"]
 
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -23,49 +23,32 @@ def search_tweets(query):
 target_tweet = search_tweets(hash_tags[0])
 
 
-def like_tweet(tweets):
+def like_retweet_and_retweet(tweets):
     for tweet in tweets:
         print(tweet)
         try:
-            print("Liking tweet...")
+            print("Liking, Retweeting and Replying tweet...")
             tweet.favorite()
-            time.sleep(10)
-        except tweepy.TweepError as e:
-            print(e.reason)
-        except StopIteration:
-            break
-
-
-def retweet(tweets):
-    for tweet in tweets:
-        print(tweet)
-        try:
-            print("Retweeting tweet...")
             tweet.retweet()
+            api.destroy_status(tweet.id)
             time.sleep(10)
         except tweepy.TweepError as e:
             print(e.reason)
         except StopIteration:
             break
+#
+#
+# # REPLY TO TWEET WITH THE HASHTAG USED
+# def reply_tweet(tweets):
+#     for tweet in tweets:
+#         try:
+#             print("Replying tweet...")
+#             api.update_status(f'#{tweet.entities["hashtags"][0]["text"]}', in_reply_to_status_id=tweet.id)
+#             time.sleep(10)
+#         except tweepy.TweepError as e:
+#             print(e.reason)
+#         except StopIteration:
+#             break
 
 
-#REPLY TO TWEET WITH THE HASHTAG USED
-def reply_tweet(tweets):
-    for tweet in tweets:
-        try:
-            print("Replying tweet...")
-            api.update_status(f'#{tweet.entities["hashtags"][0]["text"]}', in_reply_to_status_id=tweet.id)
-            time.sleep(10)
-        except tweepy.TweepError as e:
-            print(e.reason)
-        except StopIteration:
-            break
-
-
-like_tweet(target_tweet)
-retweet(target_tweet)
-reply_tweet(target_tweet)
-
-
-
-
+like_retweet_and_retweet(target_tweet)
